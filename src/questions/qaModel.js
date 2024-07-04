0bddd8173108c733616f9e914d96438b3ba7ce03
eric.roys@gmail.com <eric.roys@gmail.com>
2024-07-04 12:50:20 -0500

    initial qa mech addition"

---
+++ src/questions/qaModel.js
@@ -0,0 +1,87 @@
+
+const DEPT_ADD = 'Add a Department';
+const DEPT_DEL = 'Delete a Department';
+const DEPT_BUDGET = 'View Department Budget';
+const DEPT_ALL = 'View all Departments';
+const ROLE_ADD = 'Add a Role';
+const ROLE_DEL = 'Delete a Role';
+const ROLE_ALL = 'View All Roles';
+const EMP_ADD = 'Add an Employee';
+const EMP_DEL = 'Delete an Employee';
+const EMP_UPDATE_ROLE = 'Update Employee Role';
+const EMP_UPDATE_MGR = 'Update Employee Manager';
+const EMP_ALL = 'View All Employees';
+const EMP_BY_MGR = 'View Employees by Manager'; 
+const EMP_BY_DEPT = 'View Employees by Department';
+const QUIT = 'quit';
+
+const menOpts = [
+   DEPT_ADD,
+   DEPT_DEL,
+   DEPT_ALL,
+   DEPT_BUDGET,
+   ROLE_ADD,
+   ROLE_DEL,
+   ROLE_ALL,
+   EMP_ADD,
+   EMP_DEL,
+   EMP_ALL,
+   EMP_UPDATE_MGR,
+   EMP_UPDATE_ROLE,
+   EMP_BY_DEPT,
+   EMP_BY_MGR,
+   QUIT
+];
+
+export const QUESTIONS = 
+{
+    1: {
+        condition: ()=> true,
+        question:
+        {
+            type:'list',
+            name: 'mainChoice',
+            message: 'Main Menu',
+            choices: menOpts
+        },
+        action: null,
+        next: 2
+    },
+    2: {
+        condition: (response) => response.mainChoice === DEPT_ALL,
+        question: null,
+        action:()=>{console.log("Printed some stuff ---Dept view all")},
+        next: 1
+    },
+    3: {
+        condition: (response) => response.mainChoice === DEPT_DEL,
+        question: null,
+        action:()=>{console.log("Printed some stuff --- department delete")},
+        next: 1
+    },
+    4: {
+        condition: (response) => response.mainChoice === DEPT_ADD,
+        question: {
+            message: "Enter a Department Name",
+            name: 'deptName',
+            type: 'input',
+            validate: (value) => 
+                value.length < 1 ? "Invalid Department Name" : true,
+        },
+        action: (response) => {console.log(`${response.deptName ? "" : "Not "}Adding ${response.deptName} to the database`);
+         response.deptName = null},
+        next: 1
+    },
+    5: {
+        condition: (response) => response.mainChoice === 'quit',
+        question: null,
+        action: null,
+        next: -200
+    },
+    44: {
+        condition: ()=> true,
+        question: null,
+        action: null,
+        next: -200
+    }
+}
---
+++ src/questions/qaReader.js
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+import { QUESTIONS } from "./qaModel.js";
+
+/**
+ * Call the Q&A logic to ask the user questions in
+ * required order/condition
+ * @returns {} answers to the questions
+ */
+export async function getAnswers() {
+  let len = Object.keys(QUESTIONS).length;
+  let response = {};
+
+  //iterate the questions
+  let i = 1;
+  while(i > 0){
+    // console.log(`i=${i}, condition = ${QUESTIONS[i].condition}, question = ${JSON.stringify(QUESTIONS[i].question)}`)
+    // console.log( QUESTIONS[i].condition(response))
+
+    //add answer to the response only run if the question condition is true
+    if (QUESTIONS[i].condition(response)) {
+        // console.log("****: " + QUESTIONS[i].question.message)
+      if (QUESTIONS[i].question) {
+        response = {
+          ...response,
+          ...(await inquirer.prompt(QUESTIONS[i].question)),
+        };
+      }
+      //do the action
+      if (QUESTIONS[i].action) QUESTIONS[i].action(response);
+
+    //    console.log(`i=${i}, ${QUESTIONS[i].next}`);
+      // this provides sequencing for skipping past the conditionals
+      i = QUESTIONS[i].next;
+    } else {
+        // console.log("incrementing")
+        i++;
+    }
+    console.log(response)
+}
+  return response;
+}
+
+getAnswers();
