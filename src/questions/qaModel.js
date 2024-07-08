b243024855ecd488a8c20bdfdce9fa839527e515
eric.roys@gmail.com <eric.roys@gmail.com>
2024-07-08 15:32:25 -0500

    add role questions

--- src/questions/qaModel.js
+++ src/questions/qaModel.js
@@ -1,4 +1,5 @@
 import { department } from "../model/department.js";
+import { role } from "../model/role.js";
 import { color } from "../util/color.js";
 
 const DEPT_ADD = 'Add a Department';
@@ -43,58 +44,115 @@ export const QUESTIONS =
         {
             type: 'list',
             name: 'mainChoice',
-            message: '\n\nMain Menu',
+            message: 'Main Menu',
             choices: menOpts
         },
         action: null,
         next: 2
     },
     2: {
-        condition: (response) => response.mainChoice === DEPT_ALL,
+        condition: ({mainChoice}) => mainChoice === DEPT_ALL,
         question: null,
-        action: ()=>{
-            department.getAll().then(res => department.print(DEPT_ALL, res)).catch(err => console.error(err));
+        action: async ()=>{
+            await department.getAll().then(res => department.print(DEPT_ALL, res)).catch(err => color.error(err));
         },
         next: 1
     },
     3: {
-        condition: (response) => response.mainChoice === DEPT_DEL,
+        condition: ({mainChoice}) => mainChoice === DEPT_DEL,
         question: {
-            message: "Which Department to delete? ",
+            message: "Which Department to delete? ...",
             type: 'list',
             choices: department.getNames,
             name: 'deptName'
         },
-        action:(response)=>{
-            if(!response.deptName || response.deptName === '--none--'){
-             color.warn("no delete action to perform")
+        action: async ({deptName})=>{
+            if(!deptName || deptName === '--none--'){
+             color.warn("** no delete action to perform **")
             }
             else{
-                console.log(`Deleting ${response.deptName} from the database`)
-                department.delete(response.deptName);
-                response.deptName = null
+                await department.delete(deptName);
+                deptName = null
             }
         },
         next: 1
     },
     4: {
-        condition: (response) => response.mainChoice === DEPT_ADD,
+        condition: ({mainChoice}) => mainChoice === DEPT_ADD,
         question: {
-            message: "Enter a Department Name",
+            message: "Enter a Department Name ...",
             name: 'deptName',
             type: 'input',
             validate: (value) => 
                 value.length < 1 ? "Invalid Department Name" : true,
         },
-        action: (response) => {
-            console.log(`${response.deptName ? "" : "Not "}Adding ${response.deptName} to the database`);
-            department.create(response.deptName);
+        action: ({deptName}) => {
+            console.log(`${deptName ? "" : "Not "}Adding ${deptName} to the database`);
+            department.create(deptName);
             response.deptName = null
         },
         next: 1
     },
     5: {
-        condition: (response) => response.mainChoice === 'quit',
+        condition: (response) => response.mainChoice === ROLE_ADD,
+        question: [{
+            message: 'Enter a Role name ...',
+            type: 'input',
+            name: 'roleName',
+            validate: (value) => 
+                value.length < 1 ? "Invalid Role Name" : true,
+        },
+        {
+            message: 'Enter a Salary ...',
+            type: 'number', 
+            name: 'salary',
+        },
+        {
+            message: 'Select a Department ...',
+            type: 'list', 
+            name: 'selRoleDept',
+            choices: department.getNames
+        }],
+        action: async ({salary, selRoleDept, roleName})=>{
+            if(!salary || !selRoleDept || !roleName){
+             color.warn("** no role to add because missing input requirements **")
+            }
+            else{
+                await role.create(roleName, salary, selRoleDept).then(color.success(`Added role [${roleName}]`)).catch(err => color.error(err));
+            }
+        },
+        next: 1
+
+    },
+    6: {
+        condition: ({mainChoice}) => mainChoice === ROLE_DEL,
+        question: {
+            message: 'Select a role to delete ...',
+            type: 'list', 
+            choices: role.getNames,
+            name: 'roleName'
+        },
+        action: async ({roleName})=>{
+            if(!roleName || roleName === '--none--'){
+             color.warn("** no delete action to perform **")
+            }
+            else{
+                await role.delete(roleName);
+                roleName = null
+            }
+        },
+        next: 1
+    },
+    7: {
+        condition: ({mainChoice}) => mainChoice === ROLE_ALL,
+        question: null,
+        action: async ()=>{
+            await role.getAll().then(res => role.print(ROLE_ALL, res)).catch(err => color.error(err));
+        },
+        next: 1
+    },    
+    15: {
+        condition: ({mainChoice}) => mainChoice === 'quit',
         question: null,
         action: null,
         next: -200
