d27ed0eb8f409d665ec4e9590a0959f2134ab3f9
eric.roys@gmail.com <eric.roys@gmail.com>
2024-07-08 15:31:32 -0500

    clean up department abit

--- src/model/department.js
+++ src/model/department.js
@@ -12,12 +12,12 @@ export const department = {
   },
   delete: async (name) => {
     if (!name) return;
-    console.log("----> ", name);
+
     const query = {
       text: "delete from department where name=($1);",
       values: [name],
     };
-    return execute(query).catch((err) =>
+    return execute(query).then(res => {color.success(`Deleted department [${name}]`); return res}).catch((err) =>
       err.code === "23503"
         ? color.error(
             `Unable to delete department [${name}] because it's being used by a role`
@@ -25,6 +25,15 @@ export const department = {
         : color.error(err)
     );
   },
+  getIdByName: async (name) => {
+    if(!name) return null; 
+    const query = {
+        text: `select id from department where name=($1);`,
+        values: [name],
+    };
+    return execute(query).then(({rowCount, rows}) => rowCount > 0 ? rows[0].id : null).catch(err => color.error(err));
+  },
+
   getAll: async () => {
     const query = {
       text: `select id, name from department;`,
