1d9b2f2de9d49b6c8aabc84145bd909ef501ad3c
eric.roys@gmail.com <eric.roys@gmail.com>
2024-07-08 15:29:35 -0500

    add success color to color printer

--- src/util/color.js
+++ src/util/color.js
@@ -1,5 +1,6 @@
 
 export const color = {
     error: (text) => console.log(`\x1b[91m ${text} \x1b[0m`),
-    warn: (text) => console.log(`\x1b[93m ${text} \x1b[0m`)
+    warn: (text) => console.log(`\x1b[93m ${text} \x1b[0m`),
+    success: (text) => console.log(`\x1b[92m ${text} \x1b[0m`)
 }
