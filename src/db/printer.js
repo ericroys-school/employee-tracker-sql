2c4ff8503c85914a90020e57030fcaafc0888f9f
eric.roys@gmail.com <eric.roys@gmail.com>
2024-07-10 16:04:31 -0500

    doc added some commentary

--- src/db/printer.js
+++ src/db/printer.js
@@ -1,17 +1,28 @@
 import { color } from "../util/color.js";
 
+/**
+ * Prints the query result in table format
+ * @param {rows, fields} queryResult 
+ * @param {string} sep (optional)
+ * @param  {...number} widths (optional)
+ * @returns 
+ */
 export const print = ({rows, fields}, sep, ...widths) => {
     if(!rows || !fields) return;
     if(widths.length > 0 && fields.length !== widths.length){
         throw "Field width specification doesn't match the number of fields";
     }
+    //set defaults
     const defWidth = 40;
     const separator = sep ? sep : "\t";
     
     console.log();
+    //print the column headers
     color.statistics(fields.map((field, i) => field.name.padEnd(widths[i] || defWidth, " ")).join(separator))
+    //print the happy dash header row separator
     color.statistics(fields.map((field, i) => "-".repeat(widths[i] || defWidth)).join(separator))
     console.log()
+    //print the column values
     rows.forEach(f => {
         let x = "";
         f.forEach((r, i) => {
@@ -21,5 +32,4 @@ export const print = ({rows, fields}, sep, ...widths) => {
         color.statistics(x);
     });
     console.log("\n\n")
-   
 }
--- src/util/color.js
+++ src/util/color.js
@@ -1,8 +1,32 @@
-
+/**
+ * No fancy library to print in color, just some
+ * of the basic colorings to promote better ui
+ * experience
+ */
 export const color = {
+    /**
+     * prints text in red coloring
+     * @param {*} text 
+     * @returns 
+     */
     error: (text) => print(text, '91'),
+    /**
+     * prints text in yellow/orange coloring
+     * @param {*} text 
+     * @returns 
+     */
     warn: (text) => print(text, '93'),
+    /**
+     * prints text in greenish coloring
+     * @param {*} text 
+     * @returns 
+     */
     success: (text) => print(text, '92'),
+    /**
+     * prints text in blueish coloring
+     * @param {*} text 
+     * @returns 
+     */
     statistics: (text) => print(text, '96')
 }
 
